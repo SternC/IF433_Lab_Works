20e1a162ebfd0a5962fcd046901133d991751cee
Stern <richardsternchandra@gmail.com>
2026-02-02 16:07:03 +0700

    Week01: (tugas 2) implement discount logic

--- src/oop_000000136391_RichardSternChandra/week1/GameStore.kt
+++ src/oop_000000136391_RichardSternChandra/week1/GameStore.kt
@@ -2,6 +2,17 @@ package oop_000000136391_RichardSternChandra.week1
 
 fun main(){
     val gameTitle = "Game 1"
-    val price = 500
+    val price = 500000
 
+
+}
+
+fun calculateDiscount(price: Int) : Int {
+    var newPrice = 0
+    if (price > 500000){
+        newPrice = price - (price * 20 / 100)
+    } else {
+        newPrice = price - (price * 10 / 100)
+    }
+    return newPrice
 }
