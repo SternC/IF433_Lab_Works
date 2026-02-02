e4fc1a67daaaaaf10ce19edcb6d9a2a35165b327
Stern <richardsternchandra@gmail.com>
2026-02-02 15:17:32 +0700

    Week01: Initial setup and procedural code

---
+++ src/oop_000000136391_RichardSternChandra/week1/GradingSystem.kt
@@ -0,0 +1,8 @@
+package oop_000000136391_RichardSternChandra.week1
+
+fun main(){
+    var name: String = "John Thor"
+    var score: Int = 80
+
+    println("Nama: " + name + ", Nilai: " + score)
+}
