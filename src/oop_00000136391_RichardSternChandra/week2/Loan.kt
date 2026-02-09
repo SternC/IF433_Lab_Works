648c2526408393ff59badc02db07400e0df1835c
Stern <richardstenchandra@gmail.com>
2026-02-09 15:40:10 +0700

    week02: (task 1) create Loan class with default args

---
+++ src/oop_00000136391_RichardSternChandra/week2/Loan.kt
@@ -0,0 +1,13 @@
+package oop_00000136391_RichardSternChandra.week2
+
+class Loan (val bookTitle: String, val borrower: String, var loanDuration: Int = 1) {
+    fun calculateFine(): Int {
+        var fine: Int = 0
+        if (loanDuration > 3){
+            fine = (loanDuration - 3) * 2000
+        } else {
+            fine = 0
+        }
+        return fine
+    }
+}
