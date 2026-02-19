04fd02743b5069875fc561f800f62232ff8fdda2
Stern <richardsternchandra@gmail.com>
2026-02-19 15:08:01 +0700

    week03: (task 1) implement Weapon testing in main

---
+++ src/oop_00000136391_RichardSternChandra/week3/tugas1/Main.kt
@@ -0,0 +1,10 @@
+package oop_00000136391_RichardSternChandra.week3.tugas1
+
+fun main() {
+    val sword = Weapon("Sabit Sawit")
+
+    sword.damage = -50
+    sword.damage = 9999
+
+    println("Weapon Tier: ${sword.tier}")
+}
