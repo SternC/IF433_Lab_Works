0bd03168541c3cf84145a1d51c9d1b1e84ad5b30
Stern <richardsternchandra@gmail.com>
2026-03-03 00:09:56 +0700

    week05: (task 1) create MathHelper class with overloading

---
+++ src/oop_00000136391_RichardSternChandra/week5/tugas1/MathHelper.kt
@@ -0,0 +1,15 @@
+package oop_00000136391_RichardSternChandra.week5.tugas1
+
+class MathHelper {
+    fun hitungLuas(sisi: Int) : Int{
+        return sisi * sisi
+    }
+
+    fun hitungLuas(panjang: Int, lebar: Int) : Int {
+        return panjang * lebar
+    }
+
+    fun hitungLuas(jariJari: Double) : Double {
+        return 3.14 * jariJari * jariJari
+    }
+}
