62b1d2536db11d5453ee2b8a31fa3feb7fe554a4
Stern <richardsternchandra@gmail.com>
2026-03-03 00:12:43 +0700

    week05: (task 1) test MathHelper overloading in main

---
+++ src/oop_00000136391_RichardSternChandra/week5/tugas1/Main.kt
@@ -0,0 +1,9 @@
+package oop_00000136391_RichardSternChandra.week5.tugas1
+
+fun main() {
+    val mathHelper = MathHelper()
+
+    println(mathHelper.hitungLuas(10))
+    println(mathHelper.hitungLuas(10, 20))
+    println(mathHelper.hitungLuas(2.5))
+}
