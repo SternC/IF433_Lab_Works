723d45fb98deb6b73da344e612bdb87a44c765c9
Stern <richardsternchandra@gmail.com>
2026-03-09 15:51:29 +0700

    week06: (task) test smart home system logic in main

--- src/oop_00000136391_RichardSternChandra/week6/tugas1/Main.kt
+++ src/oop_00000136391_RichardSternChandra/week6/tugas1/Main.kt
@@ -4,4 +4,12 @@ fun main() {
     val smartLamp1 = SmartLamp("Lamp 1", "Ruang Tamu")
     val smartSpeaker1 = SmartSpeaker("Speaker 1", "Google Nest Dapur")
     val smartCCTV1 = SmartCCTV("CCTV 1", "Ezviz Garasi")
+
+    val smartHomeHub = SmartHomeHub()
+    smartHomeHub.addDevice(smartLamp1)
+    smartHomeHub.addDevice(smartSpeaker1)
+    smartHomeHub.addDevice(smartCCTV1)
+
+    smartHomeHub.activateSecurityMode()
+    smartHomeHub.turnOffAllSwitches()
 }
