40ad6aa6105c2692c8c4cb63476ebeb0c288356b
Stern <richardsternchandra@gmail.com>
2026-03-09 15:19:39 +0700

    week06: create Camera and Phone interfaces with default methods

---
+++ src/oop_00000136391_RichardSternChandra/week6/GadgetInterfaces.kt
@@ -0,0 +1,9 @@
+package oop_00000136391_RichardSternChandra.week6
+
+interface Camera {
+    fun turnOn() { println("Lensa kamera terbuka dan sensor aktif.") }
+}
+
+interface Phone {
+    fun turnOn() { println("Sinyal seluler mencari jaringan") }
+}
