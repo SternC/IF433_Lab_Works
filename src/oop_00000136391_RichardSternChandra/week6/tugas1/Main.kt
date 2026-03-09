8107fea5ef243e1c65d16d2a453118ffd5aa7acc
Stern <richardsternchandra@gmail.com>
2026-03-09 15:49:47 +0700

    week06: (task) instantiate smart devices in main

---
+++ src/oop_00000136391_RichardSternChandra/week6/tugas1/Main.kt
@@ -0,0 +1,7 @@
+package oop_00000136391_RichardSternChandra.week6.tugas1
+
+fun main() {
+    val smartLamp1 = SmartLamp("Lamp 1", "Ruang Tamu")
+    val smartSpeaker1 = SmartSpeaker("Speaker 1", "Google Nest Dapur")
+    val smartCCTV1 = SmartCCTV("CCTV 1", "Ezviz Garasi")
+}
