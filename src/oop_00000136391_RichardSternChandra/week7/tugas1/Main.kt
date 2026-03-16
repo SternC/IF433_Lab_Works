5ed96562d7b855fb6b5ed4aa5eea20ec3378a375
Stern <richardsternchandra@gmail.com>
2026-03-16 19:29:27 +0700

    week07: (task) simulate rarity and factory instantiation

--- src/oop_00000136391_RichardSternChandra/week7/tugas1/Main.kt
+++ src/oop_00000136391_RichardSternChandra/week7/tugas1/Main.kt
@@ -3,4 +3,10 @@ package oop_00000136391_RichardSternChandra.week7.tugas1
 fun main() {
     GameManager.startGame()
     GameManager.startGame()
+
+    println("Drop Chance Item Legendary: ${ItemRarity.LEGENDARY.dropChance}%")
+
+    val firstItem = Weapon.forgeStarterSword()
+    val (name, damage, rarity) = firstItem.item
+    println("Detail Senjata starter: $name | $damage | $rarity")
 }
