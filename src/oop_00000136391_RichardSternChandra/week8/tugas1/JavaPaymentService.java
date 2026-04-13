e3b103fce035c75a38861c077ed0c3d0ac8c3d1e
Stern <richardsternchandra@gmail.com>
2026-04-13 15:31:04 +0700

    week08: (task) create JavaPaymentService for payment interop

---
+++ src/oop_00000136391_RichardSternChandra/week8/tugas1/JavaPaymentService.java
@@ -0,0 +1,7 @@
+package oop_00000136391_RichardSternChandra.week8.tugas1;
+
+public class JavaPaymentService {
+    public static String processPayment(String productId){
+        return "TRX-" + productId + "-SUCCESS";
+    }
+}
