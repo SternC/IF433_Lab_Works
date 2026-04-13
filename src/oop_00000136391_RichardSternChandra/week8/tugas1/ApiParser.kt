42bd5bc11f2b0f7e37051925fd30b40669a0217d
Stern <richardsternchandra@gmail.com>
2026-04-13 15:29:22 +0700

    week08: (task) implement robust ApiParser using Elvis and requireNotNull

---
+++ src/oop_00000136391_RichardSternChandra/week8/tugas1/ApiParser.kt
@@ -0,0 +1,20 @@
+package oop_00000136391_RichardSternChandra.week8.tugas1
+
+class ApiParser {
+    fun parseProduct(rawJson: Map<String, Any?>) : Product? {
+        val id = requireNotNull(rawJson["id"] as? String) { "API Invalid! Missing ID" }
+        val name = requireNotNull(rawJson["name"] as? String) { "API Invalid! Missing Name" }
+
+        val type = rawJson["type"] as? String
+        return when (type) {
+            "ELECTRONIC" -> {
+                val warranty = rawJson["warranty"] as? Int ?: 12
+                Product.Electronic(id, name, warranty)
+            }
+            "CLOTHING" -> {
+                val size = rawJson["size"] as? String ?: "All size"
+                Product.Clothing(id, name, size)
+            } else -> null
+        }
+    }
+}
