eb89f451cd73aa5de7e23b77b6cc651faaac2116
Stern <richardsternchandra@gmail.com>
2026-05-02 00:06:18 +0700

    week10: (task) populate coin repository

--- src/oop_00000136391_RichardSternChandra/week10/tugas1/CryptoDashboard.kt
+++ src/oop_00000136391_RichardSternChandra/week10/tugas1/CryptoDashboard.kt
@@ -2,4 +2,8 @@ package oop_00000136391_RichardSternChandra.week10.tugas1
 
 fun main() {
     val coinRepo = WalletRepository<Coin>()
+    coinRepo.addItem(Coin("BTC", 5.0))
+    coinRepo.addItem(Coin("ETH", 2.5))
+    coinRepo.addItem(Coin("USDT", 3.2))
+
 }
