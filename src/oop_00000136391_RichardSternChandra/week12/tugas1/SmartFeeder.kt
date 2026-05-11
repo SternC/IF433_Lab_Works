5dc9629a749199d9e8fc2b3f2a1919be77ab747b
Stern <richardsternchandra@gmail.com>
2026-05-11 15:38:51 +0700

    week12: (task) implement hardware validation with throw

--- src/oop_00000136391_RichardSternChandra/week12/tugas1/SmartFeeder.kt
+++ src/oop_00000136391_RichardSternChandra/week12/tugas1/SmartFeeder.kt
@@ -2,4 +2,11 @@ package oop_00000136391_RichardSternChandra.week12.tugas1
 
 fun dispenseKibble(requestedGram: Int, availableGram: Int, isJammed: Boolean) : Int {
     require(requestedGram > 0) { "Porsi kibble harus lebih dari 0" }
+    if (isJammed) {
+        throw DispenserJamException()
+    }
+    if (requestedGram > availableGram) {
+        throw FoodEmptyException(requestedGram, availableGram)
+    }
+    return availableGram - requestedGram
 }
