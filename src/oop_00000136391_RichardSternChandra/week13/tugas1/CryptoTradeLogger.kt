17bf032222ec7191e301a935b9310425edf8be37
Stern <richardsternchandra@gmail.com>
2026-05-18 15:37:51 +0700

    week13: (task) implement toCsv for TradeRecord

--- src/oop_00000136391_RichardSternChandra/week13/tugas1/CryptoTradeLogger.kt
+++ src/oop_00000136391_RichardSternChandra/week13/tugas1/CryptoTradeLogger.kt
@@ -3,3 +3,5 @@ package oop_00000136391_RichardSternChandra.week13.tugas1
 data class TradeRecord(val id: Int,
                        val symbol: String, val type: String, val margin: Double, val
                        pnl: Double)
+
+fun TradeRecord.toCsv(line: String) : String = "$symbol, $type, $margin, $pnl"
